cb7ecd7cf47e4a6ccf0fc32681f7e19b1ec63226
LUIS <algo@gmail.com>
2024-05-21 11:04:28 +0200

    Se imprime un tenedor en JavaScript

---
+++ printCubierto.js
@@ -0,0 +1,9 @@
+console.log("l l l");
+console.log("l l l");
+console.log("l l l");
+console.log("l_l_l");
+console.log("  l  ");
+console.log("  l  ");
+console.log("  l  ");
+console.log("  l  ");
+console.log("  l  ");
